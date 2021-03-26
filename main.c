19f9ed78ddedbd5d4b701b0f44ef41c304232076
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2021-03-26 23:23:47 +0000

    Shared memory and exit cleanup

--- main.c
+++ main.c
@@ -2,23 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-#define DEBUG 1
+#define DEBUG 0
 #define BUFFSIZE 128
 
-int timeUnit, lapDistance, lapCount, noTeams, tBreakdown, tBoxMin, tBoxMax, capacity;
+typedef struct mem_struct {
+    int timeUnit, lapDistance, lapCount, noTeams, tBreakdown, tBoxMin, tBoxMax, capacity;
+} sharedmem;
+
+sharedmem* configs = NULL;
+int shm_key;
+
 FILE* log_file;
 
-void read_conf(char*);
-void log_message(char*);
+void init_shm();
+void read_conf(char* filename);
+void log_message(char* message);
+void terminate(int code);
 
 /* 
     ---- TODO ----
-    1. Criar memória partilhada
-    void init_shm(void);
-    
-    2. Criar processo "Gestor de Corrida", "Gestor de Equipa" e "Gestor de Avarias"
-    3. O "Gestor de Equipa" deve criar as threads "Carro"
+
+    1. Criar processo "Gestor de Corrida", "Gestor de Equipa" e "Gestor de Avarias"
+    2. O "Gestor de Equipa" deve criar as threads "Carro"
 */
 
 int main(void) {
@@ -27,100 +37,129 @@ int main(void) {
         exit(1);
     }
 
-    read_conf("config.txt");
-    log_message("I'm so tired, please\n");
+    log_message("Hello\n");
 
-    fclose(log_file);
+    init_shm(configs);
+
+    read_conf("config.txt");
+
+    terminate(0);
     return 0;
+}
+
+void init_shm() {
+    shm_key = shmget(IPC_PRIVATE, sizeof(sharedmem), IPC_EXCL|IPC_CREAT|0766);
+    if (shm_key == -1) {
+        perror("Failed to get shared memory key");
+        exit(1);
+    } else log_message("Created shared memory segment\n");
+
+    configs = (sharedmem *) shmat(shm_key, NULL, 0);
+    if (configs == NULL) {
+        perror("Failed to attach to shared memory segment");
+        exit(1);
+    } else log_message("Attached to shared memory segment\n");
 }
 
 void read_conf(char* filename) {
     FILE* config;
     if ((config = fopen(filename, "r")) == NULL) {
         perror("Failed to open config file");
-        exit(1);
+        terminate(1);
     }
 
     char buff[BUFFSIZE], *token;
     
     if (fgets(buff, sizeof(buff), config)) {
-        if (!(timeUnit = atoi(buff))) {
+        if ((configs->timeUnit = atoi(buff)) == 0) {
             fprintf(stderr, "timeUnit null\n");
             exit(1);
         }
     } else {
         fprintf(stderr, "Missing configuration arguments\n");
-        exit(1);
+        fclose(config);
+        terminate(1);
     }
 
     if (fgets(buff, sizeof(buff), config)) {
         token = strtok(buff, ",");
-        if (token == NULL || !(lapDistance = atoi(token))) {
+        if (token == NULL || (configs->lapDistance = atoi(token)) == 0) {
             fprintf(stderr, "lapDistance null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
         token = strtok(NULL, ",");
-        if (token == NULL || !(lapCount = atoi(token))) {
+        if (token == NULL || (configs->lapCount = atoi(token)) == 0) {
             fprintf(stderr, "lapCount null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
     }
 
     if (fgets(buff, sizeof(buff), config)) {
-        if (!(noTeams = atoi(buff))) {
+        if (!(configs->noTeams = atoi(buff))) {
             fprintf(stderr, "noTeams null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
     } else {
         fprintf(stderr, "Missing configuration arguments\n");
-        exit(1);
+        fclose(config);
+        terminate(1);
     }
 
     if (fgets(buff, sizeof(buff), config)) {
-        if (!(tBreakdown = atoi(buff))) {
+        if (!(configs->tBreakdown = atoi(buff))) {
             fprintf(stderr, "tBreakdown null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
     } else {
         fprintf(stderr, "Missing configuration arguments\n");
-        exit(1);
+        fclose(config);
+        terminate(1);
     }
     
     if (fgets(buff, sizeof(buff), config)) {
         token = strtok(buff, ",");
-        if (token == NULL || !(tBoxMin = atoi(token))) {
+        if (token == NULL || !(configs->tBoxMin = atoi(token))) {
             fprintf(stderr, "tBoxMin null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
         token = strtok(NULL, "\n");
-        if (token == NULL || !(tBoxMax = atoi(token))) {
+        if (token == NULL || !(configs->tBoxMax = atoi(token))) {
             fprintf(stderr, "tBoxMax null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
     }
 
     if (fgets(buff, sizeof(buff), config)) {
-        if (!(capacity = atoi(buff))) {
+        if (!(configs->capacity = atoi(buff))) {
             fprintf(stderr, "capacity null\n");
-            exit(1);
+            fclose(config);
+            terminate(1);
         }
     } else {
         fprintf(stderr, "Missing configuration arguments\n");
-        exit(1);
+        fclose(config);
+        terminate(1);
     }
 
     #if DEBUG
-        printf(">> timeUnit = %d\n", timeUnit);
-        printf(">> lapDistance = %d\n", lapDistance);
-        printf(">> lapCount = %d\n", lapCount);
-        printf(">> noTeams = %d\n", noTeams);
-        printf(">> tBreakdown = %d\n", tBreakdown);
-        printf(">> tBoxMin = %d\n", tBoxMin);
-        printf(">> tBoxMax = %d\n", tBoxMax);
-        printf(">> capacity = %d\n", capacity);
+        printf(">> timeUnit = %d\n", configs->timeUnit);
+        printf(">> lapDistance = %d\n", configs->lapDistance);
+        printf(">> lapCount = %d\n", configs->lapCount);
+        printf(">> noTeams = %d\n", configs->noTeams);
+        printf(">> tBreakdown = %d\n", configs->tBreakdown);
+        printf(">> tBoxMin = %d\n", configs->tBoxMin);
+        printf(">> tBoxMax = %d\n", configs->tBoxMax);
+        printf(">> capacity = %d\n", configs->capacity);
     #endif
 
     fclose(config);
+    log_message("Configuration read\n");
 }
 
 void log_message(char* message) {
@@ -133,4 +172,19 @@ void log_message(char* message) {
     // Print and write to log file
     fprintf(log_file, "%s %s", time_s, message);
     printf("%s %s", time_s, message);
+}
+
+void terminate(int code) {
+    if (configs) {
+        shmctl(shm_key, IPC_RMID, NULL);
+        log_message("Shared memory segment removed\n");
+        configs = NULL;
+    }
+
+    if (log_file) {
+        log_message("Goodbye\n");
+        fclose(log_file);
+        log_file = NULL;
+    }
+    exit(code);
 }
