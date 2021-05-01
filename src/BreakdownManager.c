f7ab95e5b133cc6052282a02d37bbb927201f8e2
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2021-05-01 12:40:34 +0100

    Added Named Pipe to project

--- src/BreakdownManager.c
+++ src/BreakdownManager.c
@@ -18,17 +18,17 @@ char buff[64];
 void breakdown_manager() {
     mutex = sem_open("MUTEX", 0);
     sem_wait(mutex);
-    log_message("Breakdown Manager: Process spawned\n");
-    snprintf(buff, sizeof(buff)-1, "Breakdown Manager: tBreakdown -> %d\n", configs->tBreakdown);
+    log_message("[Breakdown Manager] Process spawned\n");
+    snprintf(buff, sizeof(buff)-1, "[Breakdown Manager] tBreakdown -> %d\n", configs->tBreakdown);
     log_message(buff);
-    snprintf(buff, sizeof(buff)-1, "Breakdown Manager: tBoxMin -> %d\n", configs->tBoxMin);
+    snprintf(buff, sizeof(buff)-1, "[Breakdown Manager] tBoxMin -> %d\n", configs->tBoxMin);
     log_message(buff);
-    snprintf(buff, sizeof(buff)-1, "Breakdown Manager: tBoxMax -> %d\n", configs->tBoxMax);
+    snprintf(buff, sizeof(buff)-1, "[Breakdown Manager] tBoxMax -> %d\n", configs->tBoxMax);
     log_message(buff);
     sem_post(mutex);
     sleep(5);
     sem_wait(mutex);
-    log_message("Breakdown Manager: Process exiting\n");
+    log_message("[Breakdown Manager] Process exiting\n");
     sem_post(mutex);
     exit(0);
 }
