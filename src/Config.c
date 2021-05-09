ec179f3f1063d72dcb619486e69ae82136db9bfe
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2021-05-09 15:21:06 +0100

    Switched all printf and fprintf to logging func

--- src/Config.c
+++ src/Config.c
@@ -6,7 +6,11 @@
 extern void log_message(char* msg);
 extern void terminate(int code);
 
-void read_conf(char* filename) {
+/* 
+ * TODO If time is not of the essence, switch atoi and atof to strtol and strtod 
+ * to check for conversion errors
+ */
+int read_conf(char* filename) {
     FILE* config;
     if ((config = fopen(filename, "r")) == NULL) {
         log_message("[Race Simulator] Failed to open config file");
@@ -19,15 +23,15 @@ void read_conf(char* filename) {
     if (fgets(buff, sizeof(buff) - 1, config)) {
         in1 = atoi(buff);
         if (in1 <= 0) {
-            fprintf(stderr, "timeUnit null\n");
+            log_message("[Race Simulator] Bad config: timeUnit null");
             fclose(config);
-            return;
+            return 1;
         } else configs.timeUnit = in1;
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
@@ -36,65 +40,65 @@ void read_conf(char* filename) {
         token = strtok(NULL, "\n");
         in2 = atoi(token);
         if (in1 <= 0) {
-            printf("lapDistance is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: lapDistance is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else if (in2 <= 0) {
-            printf("lapCount is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: lapCount is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else {
             configs.lapDistance = in1;
             configs.lapCount = in2;
         }
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
         in1 = atoi(buff);
         if (in1 < 3) {
-            printf("Not enough teams to setup a race, minimum is 3\n");
+            log_message("[Race Simulator] Bad config: Not enough teams to setup a race, minimum is 3\n");
             fclose(config);
-            return;
+            return 1;
         } else configs.noTeams = in1;
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
         in1 = atoi(buff);
         if (in1 <= 0) {
-            printf("maxCars is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: maxCars is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else configs.maxCars = in1;
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
         in1 = atoi(buff);
         if (in1 <= 0) {
-            printf("tBreakdown is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: tBreakdown is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else configs.tBreakdown = in1;
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
@@ -103,38 +107,39 @@ void read_conf(char* filename) {
         token = strtok(NULL, "\n");
         in2 = atoi(token);
         if (in1 <= 0) {
-            printf("tBoxMin is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: tBoxMin is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else if (in2 < in1) {
-            printf("tBoxMax is not greater than tBoxMin\n");
+            log_message("[Race Simulator] Bad config: tBoxMax is not greater than tBoxMin\n");
             fclose(config);
-            return;
+            return 1;
         } else {
             configs.tBoxMin = in1;
             configs.tBoxMax = in2;
         }
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     if (fgets(buff, sizeof(buff) - 1, config)) {
         in1 = atoi(buff);
         if (in1 <= 0) {
-            printf("capacity is not a positive integer\n");
+            log_message("[Race Simulator] Bad config: capacity is not a positive integer\n");
             fclose(config);
-            return;
+            return 1;
         } else configs.capacity = in1;
         in1 = in2 = 0;
     } else {
-        fprintf(stderr, "Invalid configuration file\n");
+        log_message("[Race Simulator] Bad config: check file structure");
         fclose(config);
-        return;
+        return 1;
     }
 
     fclose(config);
     log_message("[Race Simulator] Configuration read");
+    return 0;
 }
